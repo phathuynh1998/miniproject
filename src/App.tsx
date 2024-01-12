b9a0180b2fbec8ffbeb27bacfb74c9a2273e1af4
phathuynh1998 <phathuynh382272510@gmail.com>
2024-01-13 00:14:43 +0700

    new css

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 import MyCV from './component/MyCV'
 // import { CiHeart } from "react-icons/ci";
