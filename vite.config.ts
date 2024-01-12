bd78d21ed246ae28388bd6513b1f6c60d65fa77e
phathuynh1998 <phathuynh382272510@gmail.com>
2024-01-13 00:54:55 +0700

    new change

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  root: "./",
+  build: {
+    outDir: "dist",
+  },
+  publicDir: "public",
   plugins: [react()],
   base: "/miniproject",
-})
+});
 
 
